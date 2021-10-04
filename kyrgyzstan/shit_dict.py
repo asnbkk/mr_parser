2796673b31849ee0d9d92edff080ec55a83f1f59
ABekkaliyev <ABekkaliyev@beeline.kz>
2021-10-04 21:52:45 +0600

    shit_dict was added, code refactoring

---
+++ kyrgyzstan/shit_dict.py
@@ -0,0 +1,26 @@
+request_data = {
+    'name': '',
+    'mnn': '',
+    'proizvod': '%%%',
+    'ftg': '',
+    'ath': '',
+    'ean': ''
+    }
+
+position_keys = [
+    'mnn',
+    'dosage_form',
+    'dosage',
+    'packing',
+    'manufacturing_company',
+    'manufacturing_country',
+    'certificate_holder',
+    'certificate_holder_country',
+    'atx',
+    'pharma_group',
+    'life_saving_medication',
+    'terms_of_dispensing',
+    'certificates_no',
+    'date_of_issue',
+    'ean'
+    ]
